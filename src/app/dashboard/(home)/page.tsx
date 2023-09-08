769b390440a911d7729e8782161ee49fd3b39fd1
dahalAvishek <dahalavishek2022@gmail.com>
2023-09-08 13:42:42 +0545

    Complete change current password functionality

--- src/app/dashboard/(home)/page.tsx
+++ src/app/dashboard/(home)/page.tsx
@@ -1,12 +1,18 @@
 'use client';
 import CardTemplate from '@/components/myCards/CardTemplate';
 import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const page = () => {
+  const router = useRouter();
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    router.push('./changeCurrentPassword');
+  };
   return (
     <div className="p-4 ">
       <CardTemplate />
+      <button onClick={(e) => handleClick(e)}>Change password</button>
       <button onClick={() => signOut()}>signout</button>
     </div>
   );
