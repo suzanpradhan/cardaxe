29b4fabd90ad6282626d06934930665c43ce3664
Avishek Dahal <dahalavishek2022@gmail.com>
2024-06-27 00:19:04 +0545

    fix: preview section

--- src/app/dashboard/(home)/builder/(layouts)/page.tsx
+++ src/app/dashboard/(home)/builder/(layouts)/page.tsx
@@ -24,6 +24,7 @@ const LayoutPage = () => {
   );
   const cardState = useSelector((state: RootState) => state.card);
 
+  console.log('cardState.cardTemplate', cardState.cardTemplate);
   return (
     <div>
       {cardTemplates?.map((card, index) => {
