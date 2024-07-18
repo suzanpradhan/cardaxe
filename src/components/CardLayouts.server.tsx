04a20e7b7b6613e49532e11b9d299f2835b87aa1
Avishek Dahal <dahalavishek2022@gmail.com>
2024-07-18 13:15:07 +0545

    fix: cardlayout border shadow

--- src/components/CardLayouts.server.tsx
+++ src/components/CardLayouts.server.tsx
@@ -49,7 +49,7 @@ const CardLayouts = ({
 
   return (
     <div
-      className={`cursor-pointer overflow-hidden rounded-lg shadow-md shadow-zinc-400`}
+      className={`cursor-pointer overflow-hidden rounded-lg shadow-lg shadow-black/5`}
     >
       {parse(processedHtml)}
     </div>
