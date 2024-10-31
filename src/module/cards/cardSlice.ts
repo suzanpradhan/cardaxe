e6a92298dc3aac6f3798bf7f6f3228a59c486939
Avishek Dahal <dahalavishek2022@gmail.com>
2024-10-31 10:42:06 +0545

    fix: card state initital card template

--- src/module/cards/cardSlice.ts
+++ src/module/cards/cardSlice.ts
@@ -55,7 +55,7 @@ export const initialState: CardState<string> = {
   cardInfos:
     { values: {}, errors: {} }
   ,
-  cardTemplate: "2"
+  cardTemplate: "1"
 
 };
 
@@ -139,7 +139,6 @@ export const cardSlice = createSlice({
         state[formName].errors = {};
       }
 
-      console.log("errorObject", state.cardBasics.errors)
 
 
     },
